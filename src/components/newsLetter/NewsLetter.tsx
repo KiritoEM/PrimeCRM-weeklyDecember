1cc21ff3b867fb8953be8289b945c88ba1b099b3
kiritoEM <johankirito64@gmail.com>
2023-12-02 19:48:48 +0100

    add title to newsletter section

--- src/components/newsLetter/NewsLetter.tsx
+++ src/components/newsLetter/NewsLetter.tsx
@@ -1,25 +1,30 @@
 const NewsLetter = () => {
-    return (
-      <section id="new-letter">
-        <div className="bg">
-          <div className="section-header">
-            <div className="row">
-              <div className="col-5">
-                <div className="title">
-                  <h3>Sell | Serve | Automate</h3>
-                  <p>
-                    Boost enrolment and manage student lifecycle with our 360°
-                    CRM
-                  </p>
-                </div>
+  return (
+    <section id="new-letter">
+      <div className="bg">
+        <div className="section-header">
+          <div className="title">
+            <h3>Sell | Serve | Automate</h3>
+            <p>
+              Boost enrolment and manage student lifecycle with our 360° CRM
+            </p>
+          </div>
+          <div className="email">
+            <p>*Your Business Email Address</p>
+            <div className="form">
+              <div className="input">
+                <input type="text" />
               </div>
-              <div className="col-7"></div>
+              <div className="button-send">
+                <button className="btn">Try Now</button>
+              </div>
             </div>
           </div>
-          <div className="section-img"></div>
         </div>
-      </section>
-    );
+        <div className="section-img"></div>
+      </div>
+    </section>
+  );
 };
 
 export default NewsLetter;
