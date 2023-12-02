c71e1561dd0db8373c985389bc5e9fa6bac0f2fd
kiritoEM <johankirito64@gmail.com>
2023-12-02 15:59:20 +0100

    add content to core-feature

--- src/components/coreFeatures/CoreFeatures.tsx
+++ src/components/coreFeatures/CoreFeatures.tsx
@@ -1,16 +1,57 @@
-const CoreFeatures = () : JSX.Element => {
-    return (
-        <section id="core-features">
-             <div className="row">
-                 <div className="col-6">
-                     <div className="section-cover">
-                           <img src="/assets/Corefeature.png" alt="" />
-                     </div>
-                 </div>
-                 <div className="col-6"></div>
-             </div>
-        </section>
-    );
+interface Ipoints {
+  text: string;
+  title: string;
+}
+
+const CoreFeatures = (): JSX.Element => {
+  const pointData: Ipoints[] = [
+    {
+      title: "Lead generation",
+      text: "Guide Helps Companies Increase Customer & Agent Satisfaction, Also Reducing Support Costs.",
+    },
+    {
+      title: "Team Email",
+      text: "Group Your Contacts By Location, Age, And Almost Any Behavior With Advanced Segmentation Tools.",
+    },
+  ];
+  return (
+    <section id="core-features">
+      <div className="row gx-4">
+        <div className="col-6">
+          <div className="section-cover">
+            <img src="/assets/Corefeature.png" alt="" />
+          </div>
+        </div>
+        <div className="col-6">
+          <div className="section-content">
+            <div className="header">
+              <h3>Allocate Leads and Simplify Lead Management</h3>
+              <p>
+                Unique and powerful suite of software to run your entire
+                business, brought to you by a company with the long term vision
+                to transform the way you work.
+              </p>
+            </div>
+            <div className="points">
+              {pointData.map((item, index) => (
+                <div className="item" key={index}>
+                  <div className="subtitle">
+                    <h5>
+                      <img src="/assets/point.png" alt="" className="mx-3" />
+                      {item.title}
+                    </h5>
+                  </div>
+                  <div className="text">
+                    <p>{item.text}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default CoreFeatures;
--- src/components/features/Features.tsx
+++ src/components/features/Features.tsx
@@ -6,7 +6,7 @@ const Features = (): JSX.Element => {
   return (
     <section id="features">
       <div className="section-header">
-        <h4>Explore Premium Features</h4>
+        <h3>Explore Premium Features</h3>
         <p>
           Unique and powerful suite of software to run your entire business,
           brought to you by a company with the long term vision to transform the
