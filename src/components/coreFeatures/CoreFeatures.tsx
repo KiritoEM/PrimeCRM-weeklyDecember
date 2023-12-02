b0433991a5219b850565a6f10697a0203c81ebf0
kiritoEM <johankirito64@gmail.com>
2023-12-02 15:31:13 +0100

    initialize core-feature section

---
+++ src/components/coreFeatures/CoreFeatures.tsx
@@ -0,0 +1,16 @@
+const CoreFeatures = () : JSX.Element => {
+    return (
+        <section id="core-features">
+             <div className="row">
+                 <div className="col-6">
+                     <div className="section-cover">
+                           <img src="/assets/Corefeature.png" alt="" />
+                     </div>
+                 </div>
+                 <div className="col-6"></div>
+             </div>
+        </section>
+    );
+};
+
+export default CoreFeatures;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Hero from "../components/hero/Hero";
 import Features from "../components/features/Features";
+import CoreFeatures from "../components/coreFeatures/CoreFeatures";
 
 const Home = (): JSX.Element => {
   return (
     <section id="home-page">
       <Hero />
       <Features />
+      <CoreFeatures />
     </section>
   );
 };
