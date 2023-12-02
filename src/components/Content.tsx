ddfb3b365b10961c3d8c91a8a84ab293a6993d1c
kiritoEM <johankirito64@gmail.com>
2023-12-02 09:30:19 +0100

    add text to content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,10 +1,12 @@
 import Color from "./content/Color";
+import Text from "./content/Text";
 
 const Content = (): JSX.Element => {
   return (
     <section id="content">
       <div id="content__container">
         <Color />
+        <Text />
       </div>
     </section>
   );
---
+++ src/components/content/Text.tsx
@@ -0,0 +1,20 @@
+const Text = (): JSX.Element => {
+  return (
+    <section id="text">
+      <div className="section-header">
+        <h2>Text</h2>
+      </div>
+      <div className="section-content">
+        <h1>Display Text</h1>
+        <h2 className="mt-4">Heading 01 Text</h2>
+        <h3 className="mt-4">Heading 02 Text</h3>
+        <h4 className="mt-4">Heading 02 Text</h4>
+        <h5 className="mt-4">Heading 02 Text</h5>
+        <h6 className="mt-4">Heading 02 Text</h6>
+        <p className="mt-4">Heading 02 Text</p>
+      </div>
+    </section>
+  );
+};
+
+export default Text;
