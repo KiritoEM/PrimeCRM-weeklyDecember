e6668e8680228c55e5111aad0ea7ec4b7ec8af4a
kiritoEM <johankirito64@gmail.com>
2023-12-02 09:59:40 +0100

    add width to column header

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,5 +1,7 @@
 import Color from "./content/Color";
 import Text from "./content/Text";
+import Paragraph from "./content/Paragraph";
+import List from "./content/List";
 
 const Content = (): JSX.Element => {
   return (
@@ -7,6 +9,8 @@ const Content = (): JSX.Element => {
       <div id="content__container">
         <Color />
         <Text />
+        <Paragraph />
+        <List />
       </div>
     </section>
   );
