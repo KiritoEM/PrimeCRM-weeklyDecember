e4770e98da031ed066a3c12f7c2a585783f2046e
kiritoEM <johankirito64@gmail.com>
2023-12-02 14:48:04 +0100

    add header to feature

---
+++ src/components/features/Features.tsx
@@ -0,0 +1,16 @@
+const Features = (): JSX.Element => {
+  return (
+    <section id="features">
+      <div className="section-header">
+        <h4>Explore Premium Features</h4>
+        <p>
+          Unique and powerful suite of software to run your entire business,
+          brought to you by a company with the long term vision to transform the
+          way you work.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Hero from "../components/hero/Hero";
+import Features from "../components/features/Features";
 
 const Home = (): JSX.Element => {
   return (
     <section id="home-page">
       <Hero />
+      <Features />
     </section>
   );
 };
